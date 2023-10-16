6bc457f891a2ea18d29c7f0699672b256dfac17f
jonasmulticloudiac <jonasmulticloudiac@gmail.com>
2023-10-16 14:53:33 +0000

    dags

--- airflow/dags/dag_airbyte_dbt.py
+++ airflow/dags/dag_airbyte_dbt.py
@@ -7,7 +7,7 @@ import json
 airbyte_economy_connection_id = Variable.get("AIRBYTE_ECONOMY_CONNECTION_ID")
 airbyte_demographics_connection_id = Variable.get("AIRBYTE_DEMOGRAPHICS_CONNECTION_ID")
 airbyte_index_connection_id = Variable.get("AIRBYTE_INDEX_CONNECTION_ID")
-airbyte_epidemiology_connection_id = Variable.get("AIRBYTE_EPIDEMIOLOGY_CONNECTION_ID")
+#airbyte_epidemiology_connection_id = Variable.get("AIRBYTE_EPIDEMIOLOGY_CONNECTION_ID")
 
 with DAG(dag_id='trigger_airbyte_dbt_job',
          default_args={'owner': 'airflow'},
@@ -33,14 +33,14 @@ with DAG(dag_id='trigger_airbyte_dbt_job',
         wait_seconds=3
     )
 
-    airbyte_epidemiology_sync = AirbyteTriggerSyncOperator(
-        task_id='airbyte_epidemiology',
-        airbyte_conn_id='airbyte_example',
-        connection_id=airbyte_epidemiology_connection_id,
-        asynchronous=False,
-        timeout=3600,
-        wait_seconds=3
-    )
+    # airbyte_epidemiology_sync = AirbyteTriggerSyncOperator(
+    #     task_id='airbyte_epidemiology',
+    #     airbyte_conn_id='airbyte_example',
+    #     connection_id=airbyte_epidemiology_connection_id,
+    #     asynchronous=False,
+    #     timeout=3600,
+    #     wait_seconds=3
+    # )
 
     airbyte_index_sync = AirbyteTriggerSyncOperator(
         task_id='airbyte_index',
@@ -51,4 +51,4 @@ with DAG(dag_id='trigger_airbyte_dbt_job',
         wait_seconds=3
     )
 
-    airbyte_economy_sync >> airbyte_demographics_sync >> airbyte_epidemiology_sync >> airbyte_index_sync
+    airbyte_economy_sync >> airbyte_demographics_sync >>  airbyte_index_sync
