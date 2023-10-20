72cb70a6b2693af47b8c25e372998a1ebd500b76
jonasmulticloudiac <jonasmulticloudiac@gmail.com>
2023-10-20 14:57:02 +0000

    dag-dummy

---
+++ airflow/dags/dag_dummy.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+
+dag = DAG('dag_dummy', description="Dummy",
+      schedule_interval=None,start_date=datetime(2023,10,17), 
+      catchup=False)
+
+
+
+
+taks1 = BashOperator(task_id="tsk1",bash_command="sleep 1",dag=dag  )      
+taks2 = BashOperator(task_id="tsk2",bash_command="sleep 1",dag=dag  )    
+taks3 = BashOperator(task_id="tsk3",bash_command="sleep 1",dag=dag )   
+taks4 = BashOperator(task_id="tsk4",bash_command="sleep 1",dag=dag  )      
+taks5 = BashOperator(task_id="tsk5",bash_command="sleep 1",dag=dag  )  
+
+
+
+
+[taks1, taks2, taks3]  >> [taks4, taks5]
